8bb9ba0b7fec0bc85e0ae21a9d97b22666cc5363
sm6142 <smetters@gmail.com>
2015-03-22 15:41:30 -0700

    Simplified version swift-only

--- TouchGame/GameScene.swift
+++ TouchGame/GameScene.swift
@@ -8,38 +8,83 @@
 
 import SpriteKit
 
+// Enumeration -- defines a variable class with five different values
+enum TouchCommand {
+    case MOVE_UP,
+    MOVE_DOWN,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    NO_COMMAND
+}
 class GameScene: SKScene {
+    let Y_DELTA:CGFloat = 30
+    let X_DELTA:CGFloat = 30
+    let NAV_FRACTION:CGFloat = 0.25
+
+    /* Properties */
+    let character = SKSpriteNode(imageNamed: "Spaceship")  // use Spaceship.png file for the image of the sprite
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        self.backgroundColor = UIColor(red:0.15, green:0.15, blue:0.3, alpha:1.0)
+        character.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+        character.setScale(0.10)
+        self.addChild(character)   // Make sprite visible
     }
-    
+
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
+        for touch in touches {
+            let command: TouchCommand = commandForTouch(touch as UITouch, node:self)
+            let moveDistance = 30
+
+            var move_x:CGFloat = 0
+            var move_y:CGFloat = 0
+
+            // Here is where you need to insert you code to set how much
+            // to move in the x direction (left / right) or the y direction (up / down)
+            if (command == TouchCommand.MOVE_UP) {
+                move_y = Y_DELTA
+            } else if (command == TouchCommand.MOVE_DOWN) {
+                move_y = -1 * Y_DELTA
+            } else if (command == TouchCommand.MOVE_LEFT) {
+                move_x = -1 * X_DELTA
+            } else if (command == TouchCommand.MOVE_RIGHT) {
+                move_x = X_DELTA
+            }
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            if (move_x != 0 || move_y != 0) {
+                let action:SKAction = SKAction.moveByX(move_x, y: move_y, duration: 0.25)
+                self.character.runAction(action)
+            }
         }
+        println("Position: \(self.character.position)")
     }
    
+    // Figures out which way the user wants to move the character based on which
+    // edge of the screen the user touched.
+    func commandForTouch(touch:UITouch, node:SKNode) -> TouchCommand {
+        let location:CGPoint = touch.locationInNode(node)
+        let frame:CGRect = node.frame
+        let height = CGRectGetHeight(frame)
+        let width = CGRectGetWidth(frame)
+
+        if (location.y/height < NAV_FRACTION) {
+            return TouchCommand.MOVE_DOWN
+        }
+        if (location.y/height > (1 - NAV_FRACTION)) {
+            return TouchCommand.MOVE_UP
+        }
+        else if (location.x/width < NAV_FRACTION) {
+            return TouchCommand.MOVE_LEFT
+        }
+        else if (location.x/width > (1 - NAV_FRACTION)) {
+            return TouchCommand.MOVE_RIGHT
+        }
+        return TouchCommand.NO_COMMAND
+    }
+
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
-}
+ }
--- TouchGame/GameViewController.swift
+++ TouchGame/GameViewController.swift
@@ -9,41 +9,17 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : NSString) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
-
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
@@ -56,11 +32,6 @@ class GameViewController: UIViewController {
         } else {
             return Int(UIInterfaceOrientationMask.All.rawValue)
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
 
     override func prefersStatusBarHidden() -> Bool {
